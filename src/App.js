3e8d39764d7c128ac43563dee4818a6bbc138cd4
Gisele Fox <>
2023-01-29 11:16:05 -0800

    adding in navbar basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// Import other js files
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <p>This is Gisele's website!</p>
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
