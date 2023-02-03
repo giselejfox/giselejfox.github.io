0f6f0ed83134eb9274101f40ff413450dc7f0da1
Gisele Fox <>
2023-02-02 16:35:18 -0800

    save all and commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 export default function HomePage() {
     return (
@@ -8,10 +9,11 @@ export default function HomePage() {
                     <h1 id="gisele-hi" className="fw-bold text-start">Hi, I'm Gisele!</h1>
                     <h2 className="fw-bold text-start">I'm an Information Professional</h2>
                     <div id="info-text">
-                        <p className="text-start" >I'll be graduating from the Informatics program at the University of Washington in June 2023 and my technical skills focus on full-stack web development and data analysis.</p>
+                        <p className="text-start" >Graduating June 2023 from the Informatics program at the University of Washington.</p>
+                        <p className="text-start">My technical skills focus on full-stack web development and data analysis.</p>
                         <p className="text-start">In my free time I like to knit sweaters, interview people, and re-work my Notion setup!</p>
                     </div>
-                    <button className="button float-start">Check out all my projects here!</button>
+                    <Button href="#" className="float-start">Check out all my projects here!</Button>
                 </div>
                 <div className="col-lg-5" id="homepage-img">
                     <img src="img/profile_drawing.jpeg" className="float-left" alt="line drawing of Gisele's face" id="homepage-image" />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,33 @@
 import React from "react";
-import { Navbar, Container, Nav, NavLink } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavBar() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
             <Container>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Gisele Fox</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Work</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Resume</a>
-                        </li>
-                        </ul>
-                    </div>
-                </nav>
+                <Navbar.Brand href="#home" className="fw-bold">
+                    <img 
+                        src="img/profile_drawing.jpeg"
+                        width="40"
+                        height="40"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Projects</Nav.Link>
+                        <Nav.Link href="#link">About</Nav.Link>
+                        <Nav.Link href="#link">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
         
     )
 }
+
+// Gisele Fox, Projects, About, Resume
