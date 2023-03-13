781f93c05354e6343a31c4c03b0b724089c3a19b
Gisele Fox <>
2023-03-13 04:43:13 -0700

    added button customization

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import { MDBFooter} from 'mdb-react-ui-kit';
 export default function Footer() {
     return(
     <MDBFooter className='text-center text-lg-start text-muted fixed-bottom'>
-        <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.03)' }}>
         © 2021 Copyright:
         <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+           MDBootstrap.com
         </a>
       </div>
     </MDBFooter>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,10 +18,10 @@ export default function NavBar() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Projects</Nav.Link>
-                        <Nav.Link href="#link">About</Nav.Link>
+                    <Nav className="container-fluid justify-content-end">
+                        <Nav.Link href="#home" className="ml-auto">Projects</Nav.Link>
                         <Nav.Link href="#link">Resume</Nav.Link>
+                        <Nav.Link href="#link">About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
