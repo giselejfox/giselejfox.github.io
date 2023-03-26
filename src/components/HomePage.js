fd655c1ac0e31f88437792df16273de104d3457f
Gisele Fox <>
2023-03-26 12:40:51 -0700

    react bootsrapified my homepage and simplified

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,29 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import { Container, Row, Col, Button } from "react-bootstrap";
 import Footer from "./Footer.js"
 import { Link } from "react-router-dom"
 
 export default function HomePage() {
     return (
-        <div className="container">
-            <div className="homepage-spread">
-                <div class='content'>
-                    <p className="text-start">Hi, my name is</p>
-                    <h1 id="homepage-name" className="fw-bold text-start">Gisele Fox</h1>
-                    <p id="homepage-intro" className="text-start">I'm a <strong>full-stack web developer</strong> and <strong>data analyst</strong> with an eye for homespun creativity.</p>
-                    <Link to="/projects">
-                        <Button className="float-start rounded-0">Check out all my projects here!</Button>
-                    </Link>
-                </div>
-                <div className="homepage-image-div">
-                    <img id="homepage-image" src="img/profile_drawing.jpeg" alt="line drawing of me"/>
-                </div>
-            </div>
-            <Footer />
-        </div>   
+        <div className="d-flex flex-column justify-content-center align-items-center nav-incl-whole-page">
+            <Container>
+                <Row>
+                    <Col lg={6} className="text-start justify-content-center d-flex flex-column">
+                        <div className="homepage-content">
+                            <p>Hi, my name is</p>
+                            <h1 id="homepage-name" className="fw-bold">Gisele Fox</h1>
+                            <p>I'm a <strong>full-stack web developer</strong> and <strong>data analyst</strong></p>
+                            <Link to="/projects">
+                                <Button variant="outline-dark" className="float-start rounded-0">Check out all my projects here!</Button>
+                            </Link>
+                        </div>
+                    </Col>
+                    <Col lg={6}>
+                        <img className="homepage-image" src="img/profile_drawing.jpeg" alt="line drawing of me"/>
+                    </Col>
+                </Row>
+                <Footer />
+            </Container>   
+        </div>
     )
 }
