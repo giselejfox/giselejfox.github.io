1dd530069aceb7e5758c8bad8edcc979b30664c6
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2023-06-27 08:03:35 -0700

    better meta tags

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import Icon from '@mdi/react';
+import { mdiEmailOutline, mdiLinkedin, mdiGithub } from '@mdi/js';
 
 
 export default function About() {
+
+    const [showCopiedEmailConfirmation, setShowCopiedEmailConfirmation] = useState(false)
+
+    function copyEmailToClipboard() {
+        navigator.clipboard.writeText("giselejfox@gmail.com")
+        setShowCopiedEmailConfirmation(true)
+        setTimeout(turnOffCopiedEmailConfirmation, 4000)
+        function turnOffCopiedEmailConfirmation() {
+            setShowCopiedEmailConfirmation(false)
+        }
+    }
+
     return (
         <div className="container about-all">
             {/* <h1 className="text-start page-header">About</h1> */}
             <Row className="px-4 pt-5">
-                <Col xs={12} md={5}>
+                <Col xs={12} md={5} className="text-start">
                     <img style={{maxHeight: "24rem", objectFit: "cover", alignSelf:"end"}} className="rounded w-100 mb-3" src="img/profile.JPG" alt="portrait of Gisele, a white androgynous looking individual with short burly hair and a striped button down" />
                 </Col>
                 <Col xs={12} md={7} className="about-content d-flex flex-column align-items-start">
+                    <h1 className="text-start end-section-lg"><div className="fw-lighter fs-3">👋 Hello! I'm </div><strong>Gisele Fox</strong></h1>
                     <h2>About Me</h2>
                     <p className="text-start end-section-lg">I'm an information professional excited about leveraging technology to enhance people's experience of the physical world, whether it be appreicating the outdoors or creating more efficient urban space.</p>
-                    <h3>Fun Facts</h3>
-                    <ul className="text-start">
+                    <h2>Fun Facts</h2>
+                    <ul className="text-start end-section-lg">
                         <li>I grew up for 5 years in Italy and can speak fluent Italian. (Ciao mi amici!)</li>
                         <li>I make my own pocket notebooks for scribbling down thoughts or doodling.</li>
                         <li>I knit and crochet my own bags and sweaters!</li>
                     </ul>
+                    <h2>Contact</h2>
+                    <div className="d-flex flow-row">
+                        <Icon 
+                            path={mdiEmailOutline} 
+                            size={1.25}
+                            onClick={copyEmailToClipboard} 
+                        />
+                        <a href="https://www.linkedin.com/in/giselejfox/" target="_blank" rel="noreferrer" >
+                            <Icon 
+                                path={mdiLinkedin}
+                                title="LikedIn"
+                                size={1.25}
+                                horizontal
+                                vertical
+                                rotate={180}
+                                color="black"
+                            />
+                        </a>
+                        <a href="https://github.com/giselejfox" target="_blank" rel="noreferrer" >
+                            <Icon 
+                                path={mdiGithub}
+                                title="Github"
+                                size={1.25}
+                                horizontal
+                                vertical
+                                rotate={180}
+                                color="black"
+                            />
+                        </a>
+                    </div>
+                    <div className="rounded p-2 px-3 mt-2" style={{backgroundColor:"#bdf5bd", display:showCopiedEmailConfirmation ? "block" : "none"}}>👍 Copied email 'giselejfox@gmail.com'</div>
                 </Col>
             </Row>
         </div>
