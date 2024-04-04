655691bbb7b2f223da62176a555143586d90d530
gisele <giselejfox@gmail.com>
2024-04-04 14:21:19 -0700

    solidify project look, archive past projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,19 +3,19 @@ import Card from 'react-bootstrap/Card';
 import { Container, Row, Col } from "react-bootstrap";
 
 import AllProjectInfo from "../data/AllProjectInfo.json"
-import { Link } from "react-router-dom";
 import gsap from 'gsap';
 
 
 export default function Projects() {
 
-    const projectCards = AllProjectInfo.map((project) => {
+    const currentProjects = AllProjectInfo.filter((project) => project.archived !== true)
+
+    const projectCards = currentProjects.map((project) => {
         return(<ProjectCard image={project.image} title={project.title} description={project.description} techText={project.techText} buttonInfo={project.buttonInfo} />)
     })
 
     return (
         <Container>
-            {/* <h1 className="page-header text-start" >Projects</h1> */}
             <Row className="project-row mt-5" xs={1} md={2} xl={3} >
                 {projectCards}
             </Row>
@@ -24,12 +24,6 @@ export default function Projects() {
 }
 
 function ProjectCard({ image, title, description, techText, buttonInfo }) {
-
-    // const projectButtons = buttonInfo.map((button) => {
-    //     return(
-    //         <ProjectCardButton link={button.link} title={button.title} />
-    //     )
-    // })
 
     const cardRef = useRef(null);
 
@@ -62,21 +56,3 @@ function ProjectCard({ image, title, description, techText, buttonInfo }) {
         </Col>
     )
 }
-
-// function ProjectCardButton({ link, title }) {
-
-
-//     return (
-//         <div>
-//             {link[0] === '/' ? 
-//                 <Link to={link} className="mb-2 me-2">
-//                     <Button variant="outline-dark" className="project-button rounded-0">{title}</Button>
-//                 </Link> 
-//             : 
-//                 <a href={link} target="_blank" rel="noreferrer" className="mb-2 me-2">
-//                     <Button variant="outline-dark" className="project-button rounded-0">{title}</Button>
-//                 </a>
-//             }
-//         </div>
-//     )
-// }
