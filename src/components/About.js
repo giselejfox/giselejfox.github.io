86f1fd08fc9da1b7c16a690c4be91f32c39be269
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-08-09 12:08:33 -0700

    content in about page and reorder pics

--- src/components/About.js
+++ src/components/About.js
@@ -1,62 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
-import Icon from '@mdi/react';
-import { mdiEmailOutline, mdiLinkedin, mdiGithub } from '@mdi/js';
+
+import aboutData from "../data/about.json"
 
 
 export default function About() {
 
-    const [showCopiedEmailConfirmation, setShowCopiedEmailConfirmation] = useState(false)
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    function copyEmailToClipboard() {
-        navigator.clipboard.writeText("giselejfox@gmail.com")
-        setShowCopiedEmailConfirmation(true)
-        setTimeout(turnOffCopiedEmailConfirmation, 4000)
-        function turnOffCopiedEmailConfirmation() {
-            setShowCopiedEmailConfirmation(false)
-        }
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setCurrentImageIndex(currentImageIndex => (currentImageIndex + 1) % 8); // 8 is how many images I have in the folder!
+        }, 2500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const dataColumn = (columnTitle) => {
+        return aboutData[columnTitle].map((textChunk) => {
+            return (
+                <div className="pt-3">
+                    {textChunk.titleLink === "" ?
+                        <h3 className="about-h3 fw-bold mb-1">{textChunk.title}</h3>
+                        :
+                        <a className="text-dark" href={textChunk.titleLink} target="_blank" rel="noreferrer" ><h3 className="about-h3 fw-bold mb-1">{textChunk.title}
+                            <span className="ps-1">
+                                <svg width="12" height="12" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.494309 12.5916C0.103785 12.9821 0.103785 13.6153 0.494309 14.0058C0.884834 14.3963 1.518 14.3963 1.90852 14.0058L0.494309 12.5916ZM13.7988 1.70129C13.7988 1.14901 13.3511 0.701294 12.7988 0.701294L3.79883 0.701294C3.24654 0.701294 2.79883 1.14901 2.79883 1.70129C2.79883 2.25358 3.24654 2.70129 3.79883 2.70129H11.7988V10.7013C11.7988 11.2536 12.2465 11.7013 12.7988 11.7013C13.3511 11.7013 13.7988 11.2536 13.7988 10.7013L13.7988 1.70129ZM1.90852 14.0058L13.5059 2.4084L12.0917 0.994187L0.494309 12.5916L1.90852 14.0058Z" fill="black"/>
+                                </svg>
+                            </span>
+                        </h3></a>
+                    }
+                    <div className="lh-sm">{textChunk.subtitle}</div>
+                    <div>{textChunk.date}</div>
+                </div>
+            )
+        })
     }
+
+
 
     return (
         <div className="container">
-            <Row className="mt-5">
-                <Col xs={12} md={5}>
-                    <h1>About Me</h1>
-                    <p>I'm an information professional currently working in Seattle as a Solutions Architect at a web agency called <a className="text-secondary" href="https://www.forumone.com">Forum One</a>. I budget projects, write technical proposals, aid in the product delivery, and help answer technical questions along the way.</p>
-                    <p>You can also find me processing my own wool at home, knitting sweaters, lifting weights, and finding the best notion setup.</p>
-                    <h2>Contact</h2>
-                    <div className="d-flex flow-row">
-                        <Icon 
-                            path={mdiEmailOutline} 
-                            size={1.25}
-                            onClick={copyEmailToClipboard} 
-                        />
-                        <a href="https://www.linkedin.com/in/giselejfox/" target="_blank" rel="noreferrer" >
-                            <Icon 
-                                path={mdiLinkedin}
-                                title="LikedIn"
-                                size={1.25}
-                                horizontal
-                                vertical
-                                rotate={180}
-                                color="black"
-                            />
-                        </a>
-                        <a href="https://github.com/giselejfox" target="_blank" rel="noreferrer" >
-                            <Icon 
-                                path={mdiGithub}
-                                title="Github"
-                                size={1.25}
-                                horizontal
-                                vertical
-                                rotate={180}
-                                color="black"
-                            />
-                        </a>
-                    </div>
-                    <div className="rounded p-2 px-3 mt-2" style={{backgroundColor:"#bdf5bd", display:showCopiedEmailConfirmation ? "block" : "none"}}>👍 Copied email 'giselejfox@gmail.com'</div>
+            <p className="intro-paragraph pt-5">Gisele Fox is a solutions analyst and web developer with a passion for creating unique, practical solutions. Their expertise spans <span className="fw-bold">custom creative coding</span>, <span className="fw-bold">technical writing, and budgeting</span>. Currently, they work as a Solutions Analyst at Forum One in the Seattle area.</p>
+            <div className="horizontal-line"></div>
+            <Row className="py-5">
+                <Col><img className="homepage-image mx-5" style={{width: "20rem", height: "30rem"}} src={"img/2min_sketches/" + currentImageIndex + ".jpeg"} alt="line drawing of me"/></Col>
+                <Col>
+                    <h2 className="about-h2">EXPERIENCE</h2>
+                    {dataColumn("experience")}
                 </Col>
-                <Col xs={12} md={7}></Col>
+                <Col>
+                    <h2 className="about-h2">EDUCATION</h2>
+                    {dataColumn("education")}
+                </Col>
+                <Col>
+                    <h2 className="about-h2">PERSONAL PROJECTS</h2>
+                    {dataColumn("personal-projects")}
+                </Col>
+                <Col>
+                    <h2 className="about-h2">CONTACT</h2>
+                    <div className="pt-3" >
+                        <h3 className="about-h3 fw-bold mb-1">EMAIL</h3>
+                        <div className="lh-sm">giselejfox@gmail.com</div>
+                    </div>
+                    <div className="pt-3" >
+                        <h3 className="about-h3 fw-bold mb-1">OTHER</h3>
+                        <a className="text-dark" href="https://www.linkedin.com/in/giselejfox/" target="_blank" rel="noreferrer" ><div>LinkedIn</div></a>
+                        <a className="text-dark" href="https://github.com/giselejfox" target="_blank" rel="noreferrer" ><div>Github</div></a>
+                    </div>
+
+                </Col>
             </Row>
         </div>
     )
