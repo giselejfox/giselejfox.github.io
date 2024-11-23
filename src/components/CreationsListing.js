988089890515134a0609ad63bbed6a8db4f3f65d
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-11-23 14:18:08 -0800

    add loading placeholder

--- src/components/CreationsListing.js
+++ src/components/CreationsListing.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import creationsData from "../data/creationsData.json"
 import { Link } from 'react-router-dom';
 
 export default function CreationsListing() {
 
+    const [imageLoading, setImageLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setImageLoading(false);
+    };
+
     const creationsElems = creationsData
         .filter((post) => post.published)
         .map((post) => {
             return (
-                <div className="col-12 col-lg-3 my-5 d-flex flex-column" key={post.id}>
+                <div className="col-12 col-lg-3 my-5 d-flex flex-column justify-content-end" key={post.id}>
                     <Link className="text-dark text-decoration-none" to={`/creations/${post.id}`}>
-                        <img className="creations-img mb-3" src={`img/creations/icons/${post.img}`} alt={post.title} />
+                        {/* Placeholder */}
+                        {imageLoading && <div className="image-placeholder">Loading...</div>}
+                        <img
+                            className="creations-img mb-3"
+                            src={`img/creations/icons/${post.img}`}
+                            alt={post.title}
+                            onLoad={handleImageLoad}
+                            style={{ display: imageLoading ? "none" : "block" }}
+                        />
                         <div className="text-decoration-none eyebrow-text">{post.publishedDate}</div>
                         <div>{post.title}</div>
                     </Link>
